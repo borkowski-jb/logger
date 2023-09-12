34abfe9d092d053882571f7a1d9eb66fbf67dd4f
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingarogroup.com>
2023-09-12 23:14:08 +0200

    first commit

---
+++ src/main/java/com/example/foo/Main.java
@@ -0,0 +1,11 @@
+package com.example.foo;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Orderer orderer = new Orderer();
+        orderer.order("John");
+        orderer.order("Jack");
+    }
+}
---
+++ src/main/java/com/example/foo/Worker.java
@@ -0,0 +1,8 @@
+package com.example.foo;
+
+//CANNOT CHANGE HERE ANYTHING
+final class Worker {
+    void work(){
+        Logger.info("Doing hard work");
+    }
+}
